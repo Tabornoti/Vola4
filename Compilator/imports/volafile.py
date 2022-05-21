77a2cfcbf28c3247a21c8702b2f1e4891019c9fc
Tabornoti <dapikg00@gmail.com>
2022-05-21 15:48:22 +0200

    Vola commit

---
+++ Compilator/imports/volafile.py
@@ -0,0 +1,4 @@
+print('Loading volafile...')
+vola_version = 4
+ide_version = 'customIDE_version'
+shsqogt = '1.2.2.331.1.5FgVOLAbdi'
---
+++ Installer/installer.py
@@ -0,0 +1,7 @@
+square = '\033[0;30;47m'
+dossc =  '\033[0;37;44m'
+rtxt = '\033[1;31;40m'
+gtxt = '\033[1;32;40m'
+btxt = '\033[1;34;40m'
+
+print(square, '           Installation of VOLA          \n')
